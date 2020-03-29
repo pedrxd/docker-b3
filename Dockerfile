ba9b1a5e0de74aaf4221be1b4be7b5166f064a30
Pedro Ruiz <peruvapedro99@gmail.com>
2020-03-30 01:14:56 +0200

    Changing data path and adding environments

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,16 @@ FROM debian:buster
 
 ENV DEBIAN_FRONTEND noninteractive
 
-ENV HOME /data
-VOLUME /data
+ENV B3_PARSER "iourt43"
+ENV B3_DATABASE "sqlite://@conf/database.sqlite"
+ENV B3_BOTNAME "b3"
+ENV B3_BOTPREFIX "^0(^2b3^0)^7:"
+ENV B3_RCONPASSWORD ""
+ENV B3_GAMEPORT 27960
+ENV B3_GAMEIP "127.0.0.1"
 
 RUN apt-get update && apt-get dist-upgrade -yq && \
-    apt-get install -yq libsqlite3-dev sqlite3 tar git curl nano wget dialog net-tools build-essential python-mysqldb python python-dev python-distribute python-pip postgresql-common libpq-dev
+    apt-get install -yq libsqlite3-dev sqlite3 tar git curl nano wget dialog net-tools build-essential python-mysqldb python python-dev python-distribute python-pip postgresql-common libpq-dev gettext-base
 
 WORKDIR /opt
 
@@ -16,6 +21,7 @@ RUN git clone https://github.com/BigBrotherBot/big-brother-bot.git /opt/b3 && \
     mv /opt/b3/b3/parsers /opt/b3/b3/.parsers && \
     pip install -r /opt/b3/requirements.txt
 
+ADD b3.xml /opt/b3/b3-docker.xml
 ADD start.sh /opt/start.sh
 RUN chmod +x /opt/start.sh
 
--- start.sh
+++ start.sh
@@ -1,30 +1,34 @@
 #!/bin/bash -e
-HOME=/data
 
-if [[ ! -d "/data/conf" ]]
-then
-   cp -rp /opt/b3/b3/.conf /data/conf
+if [ ! -e /config/b3.xml ];  then
+  echo "Server config not found, using the default one..."
+  envsubst < /opt/b3/b3-docker.xml > /config/b3.xml
 fi
-if [[ ! -d "/data/extplugins" ]]
+
+if [[ ! -d "/config/conf" ]]
 then
-   cp -rp /opt/b3/b3/.extplugins /data/extplugins
+   cp -rp /opt/b3/b3/.conf /config/conf
 fi
-if [[ ! -d "/data/parsers" ]]
+if [[ ! -d "/config/extplugins" ]]
 then
-   cp -rp /opt/b3/b3/.parsers /data/parsers
+   cp -rp /opt/b3/b3/.extplugins /config/extplugins
+fi
+if [[ ! -d "/config/parsers" ]]
+then
+   cp -rp /opt/b3/b3/.parsers /config/parsers
 fi
 
 if [[ ! -L "/opt/b3/b3/conf" ]]
 then
-   ln -s /data/conf /opt/b3/b3/conf
+   ln -s /config/conf /opt/b3/b3/conf
 fi
 if [[ ! -L "/opt/b3/b3/extplugins" ]]
 then
-   ln -s /data/extplugins /opt/b3/b3/extplugins
+   ln -s /config/extplugins /opt/b3/b3/extplugins
 fi
 if [[ ! -L "/opt/b3/b3/parsers" ]]
 then
-   ln -s /data/parsers /opt/b3/b3/parsers
+   ln -s /config/parsers /opt/b3/b3/parsers
 fi
 
-/usr/bin/python /opt/b3/b3_run.py $@
+/usr/bin/python /opt/b3/b3_run.py -c /config/b3.xml
